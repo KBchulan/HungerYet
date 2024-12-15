eb6e85589c86a162a1c6e345808d61bd819faff7
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2024-12-15 23:29:08 +0800

    small change

--- client/homedialog.cpp
+++ client/homedialog.cpp
@@ -4,9 +4,8 @@
 #include <QPainter>
 #include <QBitmap>
 
-HomeDialog::HomeDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::HomeDialog)
+HomeDialog::HomeDialog(QWidget *parent) : QDialog(parent),
+                                          ui(new Ui::HomeDialog)
 {
     ui->setupUi(this);
     setupUserInterface();
@@ -22,7 +21,7 @@ void HomeDialog::setupUserInterface()
     // 设置默认头像
     QPixmap defaultAvatar("../resources/Application/head/anonymous.png");
     setAvatar(defaultAvatar);
-    
+
     // 设置默认用户信息
     setUserInfo("未登录", "未设置邮箱");
 }
@@ -35,21 +34,22 @@ void HomeDialog::setUserInfo(const QString &username, const QString &email)
 
 void HomeDialog::setAvatar(const QPixmap &avatar)
 {
-    if (avatar.isNull()) {
+    if (avatar.isNull())
+    {
         return;
     }
 
     // 将头像处理成圆形
-    QPixmap rounded = avatar.scaled(240,240, Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
-    QPixmap mask(240,240);
+    QPixmap rounded = avatar.scaled(240, 240, Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
+    QPixmap mask(240, 240);
     mask.fill(Qt::transparent);
-    
+
     QPainter painter(&mask);
     painter.setRenderHint(QPainter::Antialiasing);
     painter.setBrush(Qt::white);
     painter.setPen(Qt::NoPen);
     painter.drawEllipse(mask.rect());
-    
+
     rounded.setMask(mask.createMaskFromColor(Qt::transparent));
     ui->avatarLabel->setPixmap(rounded);
 }
