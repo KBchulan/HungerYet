b613295197b657c61b884232d253049da4b2d4e3
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2024-12-28 16:24:52 +0800

    small change

--- client/global.cpp
+++ client/global.cpp
@@ -13,11 +13,9 @@ std::function<QString(QString)> xorString = [](QString input)
     QString result = input;
     int length = input.length();
     
-    // 使用固定密钥进行异或加密
     const QString key = "WhxChat";
     int keyLength = key.length();
     
-    // 对每个字符进行异或操作
     for(int i = 0; i < length; i++) 
         result[i] = QChar(input[i].unicode() ^ key[i % keyLength].unicode());
     
