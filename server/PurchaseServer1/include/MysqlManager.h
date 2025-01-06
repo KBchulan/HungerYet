d27253d7eceebcb0408d0f9e643f6920b5d2d9fe
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2025-01-06 14:55:29 +0800

    change

--- server/PurchaseServer1/include/MysqlManager.h
+++ server/PurchaseServer1/include/MysqlManager.h
@@ -15,16 +15,16 @@ public:
 
     // 注册新用户
     int RegUser(const std::string &name, const std::string &email, const std::string &pwd);
-    
+
     // 检查邮箱是否已被注册
     bool CheckEmail(const std::string &name, const std::string &email);
-    
+
     // 更新用户密码
     bool UpdatePwd(const std::string &name, const std::string &newpassword);
-    
+
     // 验证用户密码是否正确,并返回用户信息
     bool CheckPwd(const std::string &email, const std::string &pwd, UserInfo &userInfo);
-    
+
     // 查询用户
     std::shared_ptr<UserInfo> GetUser(int uid);
     std::shared_ptr<UserInfo> GetUser(std::string name);
